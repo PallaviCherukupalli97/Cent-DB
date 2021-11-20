b22036b05e13edb2685347c27a7d40a760245237
Jay Bhagvanbhai Sonani <jy652788@dal.ca>
2021-11-20 18:48:57 -0400

    User interface created

--- src/main/java/Authentication/LoginUser.java
+++ src/main/java/Authentication/LoginUser.java
@@ -1,4 +1,5 @@
 package Authentication;
 
 public class LoginUser {
+
 }
--- src/main/java/CentDB.java
+++ src/main/java/CentDB.java
@@ -1,7 +1,33 @@
+import UserInterface.Menu;
+
 public class CentDB {
     public static void main(String[] args) {
 
         //This is the entry point for the application
-        System.out.println("Hello World!");
+        System.out.println("Welcome to CentDB Application..!");
+        Menu menu = new Menu();
+        int choice;
+
+        while(true) {
+            choice = menu.mainMenu();
+            switch (choice) {
+                case 1 -> {
+                    System.out.println("Call Registration code");
+                    //Register new user
+                }
+                case 2 -> {
+                    System.out.println("Call Login code");
+                    // Login User
+                    
+                }
+                case 0 -> {
+                    System.out.println("Exiting application...");
+                    System.exit(0);
+                }
+                default -> {
+                    System.out.println("Invalid selection! Please try again.");
+                }
+            }
+        }
     }
 }
