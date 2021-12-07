a9b0099ea27a6b0c10a5b641b62c0ae18ae8370a
@PallaviC <pl339398@dal.ca>
2021-12-07 18:03:42 -0400

    changes

--- src/main/java/Operations/MenuOperation.java
+++ src/main/java/Operations/MenuOperation.java
@@ -1,12 +1,15 @@
 package Operations;
 
+import java.io.File;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import Dump.DumpManager;
 import LogManagement.LogManager;
+import Preferences.DatabaseSetting;
 import QueryParser.Parser;
 import UserInterface.*;
+import Transactions.Transactions;
 
 public class MenuOperation {
 
@@ -18,10 +21,41 @@ public class MenuOperation {
                 case "1":
                     Parser parser = new Parser();
                     String query = parser.takeInput();
-                    LogManager.queryLog(query);
-                    DumpManager.exportDump(query);
-                    if (parser.validQuery(query)) {
-                        parser.executeQuery(query);
+                    Transactions transactions = new Transactions();
+                    File sourceDirectory = new File(System.getProperty("user.dir") + "/assets/database/" + DatabaseSetting.SELECTED_DATABASE);
+                    File targetDirectory = new File(System.getProperty("user.dir") + "/assets/database/transactionDatabase");
+                    if(query.toLowerCase().startsWith("begin"))
+                    {
+                        transactions.copyDatabase(sourceDirectory, targetDirectory);
+                        transactions.changeGlobalDb();
+                        //take input
+                        while(true){
+                            if(query.toLowerCase().startsWith("commit") || query.toLowerCase().startsWith("rollback")) {
+                                if(query.toLowerCase().startsWith("commit"))
+                                {
+                                    // replace original db with transactiondb
+                                    transactions.copyDatabase(targetDirectory, sourceDirectory);
+                                    System.out.println("Transaction committed successfully");
+                                }else{
+                                    System.out.println("Transaction rollback successfully");
+                                }
+                                break;
+                            }
+                            query = parser.takeInput();
+                            if(!query.toLowerCase().startsWith("commit")) {
+                                parser.executeQuery(query);
+                            }
+                        }
+                       //delete trans db
+                        transactions.deleteTransactionDatabase();
+                    }
+                    else
+                    {
+                        LogManager.queryLog(query);
+                        DumpManager.exportDump(query);
+                        if (parser.validQuery(query)) {
+                            parser.executeQuery(query);
+                        }
                     }
                     break;
 
--- src/main/java/QueryParser/Parser.java
+++ src/main/java/QueryParser/Parser.java
@@ -2,9 +2,7 @@ package QueryParser;
 
 import Operations.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
 import Preferences.*;
 
@@ -12,28 +10,8 @@ public class Parser {
     public String takeInput() {
         System.out.println("Enter a query here: ");
         Scanner sc = new Scanner(System.in);
-        String query = new String();
-        while (sc.hasNextLine()) {
-            String input = sc.nextLine();
-            if (input.toLowerCase().startsWith("begin") || query.toLowerCase().startsWith("begin")) {
-                if (input.toLowerCase().startsWith("end") || input.toLowerCase().startsWith("commit")) {
-                    query = query.concat(input);
-                    break;
-                }
-                query = query.concat(input);
-                if (query.length() != 0) {
-                    query = query.concat("\r\n");
-                }
-            }
-            else {
-                if(query.length() == 0)
-                {
-                    query = query.concat(input);
-                    break;
-                }
-            }
-        }
-        return query;
+        String input = sc.nextLine();
+        return input;
     }
 
     public boolean validQuery(String query) {
@@ -153,22 +131,6 @@ public class Parser {
                     System.out.println("Invalid query. Please try again.");
                 }
 
-                break;
-
-            case "BEGIN":
-                if (Validator.validateTransactionQuery(query)) {
-                    if (DatabaseSetting.SELECTED_DATABASE == null) {
-                        System.out.println("Database not selected");
-                    } else {
-                        String[] queryLines = query.split("\r\n");
-                        for (int i = 1; i < queryLines.length; i++) {
-                            if (!(queryLines[i].split(" ")[0]).toLowerCase().startsWith("commit") && !(queryLines[i].split(" ")[0]).toLowerCase().startsWith("end")) {
-                                executeQuery(queryLines[i]);
-                            }
-                        }
-
-                    }
-                }
                 break;
 
             default:
