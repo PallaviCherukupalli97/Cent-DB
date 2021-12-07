b60d0a59620d6cac97360a0802e1aa50fde0c090
Jay Bhagvanbhai Sonani <jy652788@dal.ca>
2021-12-07 18:28:22 -0400

    Analytics fixed

--- src/main/java/Operations/MenuOperation.java
+++ src/main/java/Operations/MenuOperation.java
@@ -62,11 +62,6 @@ public class MenuOperation {
 //                Data Dictionary
                     break;
 
-                case "6":
-                    System.out.println("Implement logout feature here");
-//                logout
-                    break;
-
                 default:
                     System.out.println("Invalid choice. Please try again");
             }
